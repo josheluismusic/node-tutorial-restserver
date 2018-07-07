3472064ef351f06b373259230e91ea10a1a2d1d8
José Luis Candia Figueroa <josheluis.music@gmail.com>
2018-07-07 13:10:30 -0400

    set variable HEROKU

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let dbUrl;
 if(process.env.NODE_ENV == 'dev') {
     dbUrl = 'mongodb://localhost:27017/cafe'
 } else {
-    dbUrl = 'mongodb://cafe-user:cafe-user123456@ds131551.mlab.com:31551/cafe-dev'
+    dbUrl = process.env.MONGO_URI
 }
 
 process.env.DB_URL = dbUrl;
